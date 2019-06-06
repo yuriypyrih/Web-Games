b0bd4b91e794431fe5b27f31beade9eb2bada996
yuriypyrih <uripirig@gmail.com>
2019-06-06 15:01:31 +0300

    Victory and Defeat Screens for game 2

--- games2.php
+++ games2.php
@@ -17,36 +17,73 @@ if(!isset($_SESSION["username"])){
 </head>
 <body>
     <div id="display">
+        <div id="upper_div">
 
-    <p id="demo"></p> 
-    <a class="games2_exit hover-btn" href="main.php">ΕΞΩΔΟΣ</a>
-
-        <script>
-
-            var context, input;
-            var images = ["dog", "cat", "bird", "train", "car", "trumpet"];
-
-            context = document.getElementById("display");
-
-            
-
-            for(i = 0; i < 6; i++){
-                input = document.createElement("input");
-                input.setAttribute('type', 'image');
-                input.setAttribute('class', 'games2_images');
-                input.setAttribute('src', 'img/' + images[i] + '.png');
-                input.setAttribute('alt', images[i]);
-
-                context.appendChild(input);
-            }
-            
-            var audio = new Audio('audio/dog.mp3');
-           // audio.play();
+             <p id="timer"></p> 
+             <a class="games2_exit hover-btn" href="main.php">ΕΞΟΔΟΣ</a>
+             
+             <script> 
+             var deadline = new Date(); 
+             deadline.setMinutes(deadline.getMinutes() + 1);
+             deadline.setSeconds(deadline.getSeconds() + 31);
+             deadline = deadline.getTime()
+             var x = setInterval(function() { 
+             var now = new Date().getTime(); 
+             var t = deadline - now; 
+             var minutes = Math.floor((t % (1000 * 60 * 60)) / (1000 * 60)); 
+             var seconds = Math.floor((t % (1000 * 60)) / 1000); 
+             document.getElementById("timer").innerHTML =  minutes + ":" + seconds ; 
+                 if (t < 0) { 
+                     clearInterval(x); 
+                     //document.getElementById("demo").innerHTML = "EXPIRED"; 
+                 } 
+             }, 1000); 
+             </script> 
 
             
-            
-        </script>
-       <!-- <input type="image" class="games2_images" name="imgbtn" src="img/dog.png" alt="png"> -->
+
+        </div>
+        <div id="lower_div">
+            <script>
+
+                var context, input, link,  audio, keyword;
+                solution = "dog";
+                audio = new Audio('audio/' + solution +'.mp3');
+                var images = ["dog", "cat", "bird", "train", "car", "trumpet"];
+
+                context = document.getElementById("display");
+
+
+
+                for(i = 0; i < 6; i++){
+
+                    link = document.createElement("a");
+                    if(solution===images[i]){
+                        link.setAttribute('href', "victory.php"); 
+                    }
+                    else{
+                        link.setAttribute('href', "defeat.php"); 
+                    }
+
+                    input = document.createElement("input");
+                    input.setAttribute('type', 'image');
+                    input.setAttribute('class', 'games2_images');
+                    input.setAttribute('src', 'img/' + images[i] + '.png');
+                    input.setAttribute('alt', images[i]);
+
+                    link.appendChild(input);
+
+                  
+                    context.appendChild(link);
+                }
+
+                
+               // audio.play();
+
+
+
+            </script>
+        </div>
     </div>
 </body>
 </html>
---
+++ victory.php
@@ -0,0 +1,26 @@
+<?php
+include("session.php"); // Include Login Script
+
+//Delete this echo later
+echo " Your session is " . $_SESSION["username"];
+
+if(!isset($_SESSION["username"])){
+   // header("location: index.php"); //Redirecting to index
+}
+?>
+<!DOCTYPE HTML>
+<html>
+<head>
+<link rel="stylesheet" href="canvas.css" type="text/css" media="screen" />
+<link rel="stylesheet" href="games.css" type="text/css" media="screen" />
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+</head>
+<body>
+    <div id="display">
+       <p id="header_tittle">Μπράβο!</p>
+       <img id="icon_endscreen" src="img/green-tick-png.png" alt="Green Tick">
+       <a  style="left:100px;" class="games2_endscreen_btn hover-btn" href="games2.php">ΈΠΟΜΕΝΟ</a>
+       <a  style="left:480px;" class="games2_endscreen_btn hover-btn" href="games.php">ΈΞΟΔΟΣ</a>
+     </div>
+</body>
+</html>
